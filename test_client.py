974f26422703b6b140e1c1cb14d1e9f98573bfb4
Dmitriy Zhiltsov <dmitry@pushamp.com>
2017-04-11 11:34:00 +0300

    w

--- test_client.py
+++ test_client.py
@@ -2,7 +2,7 @@ import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client.connect(('51.15.56.6', 6767))
+client.connect(('0.0.0.0', 6767))
 payload = '{"ts": 186401, "app_name": "09", "app_layer": "we", "value": 4.4332, "operation1": "select", "metric_name": "2323", "labels": {"app_name": "3443","HOST": "host1", "port": "0987"}}\n'
 data = (payload * 3).encode() 
 while True:
